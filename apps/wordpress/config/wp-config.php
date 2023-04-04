b7bacd39484f0c3be84ac35a64b5fecb5a3bf780
FILIP Iulian <ifilip@gmail.com>
2023-04-04 19:42:27 +0300

    mysql update #11

--- apps/wordpress/config/wp-config.php
+++ apps/wordpress/config/wp-config.php
@@ -95,3 +95,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
+
+define( 'WP_HOME', 'http://192.168.56.10' );
+define( 'WP_SITEURL', 'http://192.168.56.10' );
